2708570a82fdc9fd1a22403bec1782c7c659dde0
Adaptolog <yaroslawx@gmail.com>
2025-03-12 18:29:41 +0200

    main.py: Add function to solve quadratic equations
    - Implemented solve_quadratic(a, b, c) to compute roots.

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
- 
+import sys
+import math
+
+def solve_quadratic(a, b, c):
+    if a == 0:
+        print("Error. a cannot be 0", file=sys.stderr)
+        sys.exit(1)
+
+    D = b**2 - 4*a*c
+    if D > 0:
+        x1 = (-b + math.sqrt(D)) / (2*a)
+        x2 = (-b - math.sqrt(D)) / (2*a)
+        return [x1, x2]
+    elif D == 0:
+        x = -b / (2*a)
+        return [x]
+    else:
+        return []
