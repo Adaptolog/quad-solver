0f0ffcec3d54258f3aacb764b387c698845dba44
Adaptolog <yaroslawx@gmail.com>
2025-03-12 18:32:11 +0200

    main.py: Implement interactive mode
    - Added command-line interface for user input.

--- main.py
+++ main.py
@@ -23,3 +23,15 @@ def get_float_input(prompt):
             return float(input(prompt))
         except ValueError as e:
             print(f"Error. Expected a valid real number, got {e.args[0]} instead")
+
+if __name__ == "__main__":
+    a = get_float_input("a = ")
+    b = get_float_input("b = ")
+    c = get_float_input("c = ")
+
+    roots = solve_quadratic(a, b, c)
+    print(f"Equation is: ({a}) x^2 + ({b}) x + ({c}) = 0")
+    print(f"There are {len(roots)} roots")
+
+    for i, root in enumerate(roots, start=1):
+        print(f"x{i} = {root}")
