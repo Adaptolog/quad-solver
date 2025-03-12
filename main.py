a36a726e8866ff7926253176b8506ca8f4db5b33
Adaptolog <yaroslawx@gmail.com>
2025-03-12 18:37:00 +0200

    main.py : Final cleanup and optimization
    - Refactored code for better readability and efficiency.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import math
 
 def solve_quadratic(a, b, c):
     if a == 0:
-        print("Error: Coefficient 'a' cannot be 0 (division by zero)", file=sys.stderr)
+        print("Error. a cannot be 0", file=sys.stderr)
         sys.exit(1)
 
     D = b**2 - 4*a*c
@@ -12,7 +12,8 @@ def solve_quadratic(a, b, c):
         x2 = (-b - math.sqrt(D)) / (2*a)
         return [x1, x2]
     elif D == 0:
-        return [-b / (2*a)]
+        x = -b / (2*a)
+        return [x]
     else:
         return []
 
@@ -29,11 +30,26 @@ if __name__ == "__main__":
         b = get_float_input("b = ")
         c = get_float_input("c = ")
 
+        roots = solve_quadratic(a, b, c)
+        print(f"Equation is: ({a}) x^2 + ({b}) x + ({c}) = 0")
+        print(f"There are {len(roots)} roots")
+
+        for i, root in enumerate(roots, start=1):
+            print(f"x{i} = {root}")
+
     elif len(sys.argv) == 2:
         try:
             with open(sys.argv[1], "r") as file:
                 line = file.readline().strip()
                 a, b, c = map(float, line.split())
+
+                roots = solve_quadratic(a, b, c)
+                print(f"Equation is: ({a}) x^2 + ({b}) x + ({c}) = 0")
+                print(f"There are {len(roots)} roots")
+
+                for i, root in enumerate(roots, start=1):
+                    print(f"x{i} = {root}") 
+
         except FileNotFoundError:
             print(f"file {sys.argv[1]} does not exist", file=sys.stderr)
             sys.exit(1)
@@ -43,10 +59,3 @@ if __name__ == "__main__":
     else:
         print("Usage: python main.py [filename]", file=sys.stderr)
         sys.exit(1)
-
-    roots = solve_quadratic(a, b, c)
-    print(f"Equation is: ({a}) x^2 + ({b}) x + ({c}) = 0")
-    print(f"There are {len(roots)} roots")
-
-    for i, root in enumerate(roots, start=1):
-        print(f"x{i} = {root}")
